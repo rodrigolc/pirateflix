93bd2f89e47f09902d3fd9093229d74881f1e493
Rodrigo Lopes de Carvalho <rodrigo@rodrigo.lc>
2016-04-26 22:39:00 -0300

    added colors

--- pirateflix.py
+++ pirateflix.py
@@ -1,5 +1,49 @@
 #!/usr/bin/env python
 
+#color class from http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+class colors:
+    '''Colors class:
+    reset all colors with colors.reset
+    two subclasses fg for foreground and bg for background.
+    use as colors.subclass.colorname.
+    i.e. colors.fg.red or colors.bg.green
+    also, the generic bold, disable, underline, reverse, strikethrough,
+    and invisible work with the main class
+    i.e. colors.bold
+    '''
+    reset='\033[0m'
+    bold='\033[01m'
+    disable='\033[02m'
+    underline='\033[04m'
+    reverse='\033[07m'
+    strikethrough='\033[09m'
+    invisible='\033[08m'
+    class fg:
+        black='\033[30m'
+        red='\033[31m'
+        green='\033[32m'
+        orange='\033[33m'
+        blue='\033[34m'
+        purple='\033[35m'
+        cyan='\033[36m'
+        lightgrey='\033[37m'
+        darkgrey='\033[90m'
+        lightred='\033[91m'
+        lightgreen='\033[92m'
+        yellow='\033[93m'
+        lightblue='\033[94m'
+        pink='\033[95m'
+        lightcyan='\033[96m'
+    class bg:
+        black='\033[40m'
+        red='\033[41m'
+        green='\033[42m'
+        orange='\033[43m'
+        blue='\033[44m'
+        purple='\033[45m'
+        cyan='\033[46m'
+        lightgrey='\033[47m'
+
 #python version compatibility
 try:
     from urllib.request import urlopen,Request
@@ -53,8 +97,9 @@ for r in result_list[1:]: #first is header
     m = m.groups()
     result = {"name": m[0], "magnet":m[1],"description":unescape(m[3]+m[4]),"VIP": True if 'alt="VIP"' in m[2] or 'alt="Trusted"' in m[2] else False}
     results.append(result)
-    print "[%s]>>>" % (i), result["name"]
-    print result["description"], "[VIP]" if result["VIP"] else ""
+    print (colors.fg.green if result["VIP"] else "") + result["description"]
+    print "[%s]>>>" % (i), result["name"],colors.reset
+
     i+=1
 
 choice = ""
@@ -65,7 +110,7 @@ while(error):
         if re.match("^[0-9]+",choice):
             choice = int(choice)
             error = False
-            
+
         elif choice == "q" :
             sys.exit(0)
         elif re.match("[m]+ [0-9]*",choice):
