dc9bef47ea00f7a4a04e8ed71c56463d7113dfee
Rodrigo Lopes <rodrigo@Rodrigos-MacBook-Pro.local>
2017-05-30 03:26:22 -0300

    passed project through flake8 and corrected stuff

--- colors.py
+++ colors.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-#color class from http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+# color class from
+# http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+
+
 class colors:
     '''Colors class:
     reset all colors with colors.reset
@@ -11,37 +14,37 @@ class colors:
     and invisible work with the main class
     i.e. colors.bold
     '''
-    reset='\033[0m'
-    bold='\033[01m'
-    disable='\033[02m'
-    underline='\033[04m'
-    reverse='\033[07m'
-    strikethrough='\033[09m'
-    invisible='\033[08m'
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m'
+
     class fg:
-        black='\033[30m'
-        red='\033[31m'
-        green='\033[32m'
-        orange='\033[33m'
-        blue='\033[34m'
-        purple='\033[35m'
-        cyan='\033[36m'
-        lightgrey='\033[37m'
-        darkgrey='\033[90m'
-        lightred='\033[91m'
-        lightgreen='\033[92m'
-        yellow='\033[93m'
-        lightblue='\033[94m'
-        pink='\033[95m'
-        lightcyan='\033[96m'
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+
     class bg:
-        black='\033[40m'
-        red='\033[41m'
-        green='\033[42m'
-        orange='\033[43m'
-        blue='\033[44m'
-        purple='\033[45m'
-        cyan='\033[46m'
-        lightgrey='\033[47m'
-
-
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m'
--- pirateflix.py
+++ pirateflix.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 
-#python version compatibility
+# python version compatibility
 
 try:
-    from urllib.request import urlopen,Request
+    from urllib.request import urlopen, Request
 except ImportError:
     from urllib2 import urlopen
     from urllib2 import Request
-import sys
 import re
+import subprocess
+import sys
+
+import colors
+
 try:
     from http import unescape
 except ImportError:
     try:
         import html.parser
+
         def unescape(string):
             return html.parser.HTMLParser().unescape(string)
     except ImportError:
         import HTMLParser
+
         def unescape(string):
             return HTMLParser.HTMLParser().unescape(string)
 
-from colors import colors
 url_base = "http://thepiratebay.org/search/"
 if "-t" in sys.argv[1]:
     test = True
@@ -29,15 +34,19 @@ if "-t" in sys.argv[1]:
 else:
     query = sys.argv[1:]
     test = False
-args = " ".join(query) # will the " " break this?
+args = " ".join(query)  # will the " " break this?
 url = url_base + args
 if not test:
-    print("searching for torrents on piratebay using \"%s\" as search terms" % (args))
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"}
-request = Request(url,headers=headers)
+    print("searching for torrents on piratebay using \"%s\" as search terms" %
+          (args))
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) " +
+    "AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"}
+request = Request(url, headers=headers)
 page = urlopen(request).read()
 
-result_table = re.findall("<table id=\"searchResult\">(.*)</table>",page, re.M + re.S)
+result_table = re.findall(
+    "<table id=\"searchResult\">(.*)</table>", page, re.M + re.S)
 
 if test:
     sys.exit(0 if len(result_table) > 0 else 1)
@@ -47,18 +56,25 @@ if len(result_table) < 1:
     sys.exit(1)
 
 result_table = result_table[0]
-result_list = re.findall("<tr[^>]*>(.*?)</tr>",result_table,re.M+re.S)
+result_list = re.findall("<tr[^>]*>(.*?)</tr>", result_table, re.M + re.S)
 results = []
 i = 0
-for r in result_list[1:]: #first is header
-    m = re.search('detName">.*?>(.*?)</a>.*?<a href="(.*?)"(.*)"detDesc">(.*?)<.*?>(.*?)<',r,re.M + re.S)
+for r in result_list[1:]:  # first is header
+    m = re.search(
+        'detName">.*?>(.*?)</a>.*?' +
+        '<a href="(.*?)"(.*)"detDesc">(.*?)<.*?>(.*?)<',
+        r, re.M + re.S)
     m = m.groups()
-    result = {"name": m[0], "magnet":m[1],"description":unescape(m[3]+m[4]),"VIP": True if 'alt="VIP"' in m[2] or 'alt="Trusted"' in m[2] else False}
+    result = {"name": m[0], "magnet": m[1], "description": unescape(
+        m[3] + m[4]), "VIP": True if 'alt="VIP"' in m[2] or
+                                     'alt="Trusted"' in m[2]
+                                     else False}
     results.append(result)
-    print (colors.fg.green if result["VIP"] else "") + "[%s]>>>" % (i), result["name"]
-    print result["description"],colors.reset
+    print (colors.fg.green if result["VIP"]
+           else "") + "[%s]>>>" % (i), result["name"]
+    print result["description"], colors.reset
 
-    i+=1
+    i += 1
 
 choice = ""
 error = True
@@ -66,40 +82,43 @@ index = False
 vlc = True
 while(error):
     try:
-        choice = raw_input("Which torrent to play(q to quit, m INDEX to show magnet link, l INDEX to list available files in torrent)?\n:")
-        if re.match("^[0-9]+",choice):
+        choice = raw_input(
+            "Which torrent to play('q' to quit, 'm INDEX' to show magnet" +
+            " link 'l INDEX' to list available files in torrent)?\n:")
+        if re.match("^[0-9]+", choice):
             choice = int(choice)
             error = False
 
-        elif choice == "q" :
+        elif choice == "q":
             sys.exit(0)
-        elif re.match("[l]+ [0-9]*",choice):
-            g = re.findall("[l]+ [0-9]*",choice)
+        elif re.match("[l]+ [0-9]*", choice):
+            g = re.findall("[l]+ [0-9]*", choice)
             choice = int(g[0].split()[1])
             index = True
             error = False
-        elif re.match("[m]+ [0-9]*",choice):
-            g = re.findall("[m]+ [0-9]*",choice)
+        elif re.match("[m]+ [0-9]*", choice):
+            g = re.findall("[m]+ [0-9]*", choice)
             choice = int(g[0].split()[1])
-            print ("%s:\n%s"%(results[choice]["name"],results[choice]["magnet"]))
+            print ("%s:\n%s" %
+                   (results[choice]["name"], results[choice]["magnet"]))
             sys.exit(0)
             error = False
-        elif re.match("[d]+ [0-9]*",choice):
-            g = re.findall("[d]+ [0-9]*",choice)
+        elif re.match("[d]+ [0-9]*", choice):
+            g = re.findall("[d]+ [0-9]*", choice)
             choice = int(g[0].split()[1])
             error = False
             vlc = False
-    except Exception,e:
+    except Exception, e:
         print(e)
         print("Invalid number. Try again")
 
 print ("opening peerflix using %s" % (results[int(choice)]["name"]))
 
-import subprocess
 peerflix_options = ["-d"]
 if vlc:
     peerflix_options.append("-v")
 if index:
     peerflix_options.append("-l")
 
-ret = subprocess.call(["peerflix"] + peerflix_options +[results[choice]["magnet"]],stdout=1)
+ret = subprocess.call(["peerflix"] + peerflix_options +
+                      [results[choice]["magnet"]], stdout=1)
